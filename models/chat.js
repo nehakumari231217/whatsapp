076851e9b3680f78fbb08630e989b5f6defb4441
Neha kumari <141753244+nehakumari231217@users.noreply.github.com>
2024-05-04 00:06:02 +0530

    Add files via upload

---
+++ models/chat.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const chatschema = new mongoose.Schema({
+    from: {
+        type:String,
+        required:true
+    },
+    to: {
+        type: String,
+        required:true
+    },
+    msg: {
+        type: String,
+        maxLength: 50,
+    },
+    created_at: {
+        type: Date,
+        required:true
+    },
+});
+
+const chat = mongoose.model("chat", chatschema);
+
+module.exports = chat;
